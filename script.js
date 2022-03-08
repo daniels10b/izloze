c120365291b358affaa83bf2c52698bf7adb8720
daniels10b <surfdaburf69@gmail.com>
2022-03-08 11:14:32 +0200

    Stundas darbs

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-const vardi=['Lauris Birge', 'Jana Svetlana','Nauris Normunds','Oļegs Birzīts','Paula Svilpe'];
-const balvas=['Māja','Mašīna','Laiva','Dators','Ceļojums'];
-const naudasKopa=100000000;
-let uzvaretajuSkaits=5;
-Math.random()*10;
+const vardi = ['Lauris Bigauns', 'Jana Svetlana', 'Nauris Normunds', 'Osis Birze', 'Paula Svilpe'];
+const balvas = ['Maja', 'Masina', 'Laiva', 'Dators', 'Celojums'];
+const naudasKopa = 100000000;
+let uzvaretajuSkaits = 5;
+let rindas=document.querySelector('.rindas');//pievieno mainīgo
+
+for (let i = 0; i < uzvaretajuSkaits; i++) {
+
+    let rand = Math.random() * vardi.length;//vārdu skaits mainīgs
+    rand = Math.floor(rand);//noapaļo uz leju
+    let uzvaretajs=vardi[rand];//console.log vietā
+}
+rindas.innerHTML+=`
+<tr>
+<td>${i+1}</td>
+<td>${uzvaretajs}</td>
+</tr>` //izvade vairākās rindas
 
 
 
-
-
-
-
-
-console.log(vardi[rand]);
