bea2a9694e3d91f9058589e7b759273df24f45f0
daniels10b <surfdaburf69@gmail.com>
2022-03-08 10:18:17 +0200

    Pirmais

---
+++ script.js
@@ -0,0 +1,14 @@
+const vardi=['Lauris Birge', 'Jana Svetlana','Nauris Normunds','Oļegs Birzīts','Paula Svilpe'];
+const balvas=['Māja','Mašīna','Laiva','Dators','Ceļojums'];
+const naudasKopa=100000000;
+let uzvaretajuSkaits=5;
+Math.random()*10;
+
+
+
+
+
+
+
+
+console.log(vardi[rand]);
